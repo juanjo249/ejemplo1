a5556964620bb55afb36ba136e2345625399689d
Juanjo <Juanjo@LAPTOP-PVOG9V52>
2021-03-07 11:13:04 +0100

    ejemplo

---
+++ Desktop/Carpeta_Tarea/Deposito/src/Cuentas/Main.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Cuentas;
+
+/**
+ *
+ * @author Juanjo
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+          CCuenta cuenta1;
+        double saldoActual;
+        cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
+        saldoActual = cuenta1.estado();
+        System.out.println("El saldo actual es"+ saldoActual );
+        
+      
+       // operativa_cuenta(cuenta1, null);
+    }  
+
+    public static void operativa_cuenta(CCuenta cuenta1, float cantidad) {
+        try {
+            cuenta1.retirar(2300);
+        } catch (Exception e) {
+            System.out.print("Fallo al retirar");
+        }
+        try {
+            System.out.println("Ingreso en cuenta");
+            cuenta1.ingresar(695);
+        } catch (Exception e) {
+            System.out.print("Fallo al ingresar");
+        }
+    }  
+    
+}
